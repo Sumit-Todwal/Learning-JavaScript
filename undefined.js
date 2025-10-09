cfb47303edf664ed8f70ac545f6b7a7846fe237a
Sumit Todwal <todwalsumit@gmail.com>
2025-10-09 17:10:38 +0530

    undefined-notdefined

---
+++ undefined.js
@@ -0,0 +1,20 @@
+console.log(a);
+var a=7;
+console.log(a);
+if(a ===  undefined)
+{
+    console.log("a is undefined");
+}
+else{
+    console.log(" a is not undefined");
+}
+
+// JS is loosely typed language since it can handle multiple datatypes for same variable.
+var a;
+console.log(a);
+a = 56;
+console.log(a);
+a = "Hello world";
+console.log(a);
+
+
