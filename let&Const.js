1b5bed4c22cce5334aa80e834b468e8e7599f9ce
Sumit Todwal <todwalsumit@gmail.com>
2025-10-10 18:32:02 +0530

    Let&Const in JS

---
+++ let&Const.js
@@ -0,0 +1,14 @@
+let a = 5;
+console.log(a);
+// var b = 10;
+// var b = 15;
+// console.log(b);
+// let a = 15; // Redeclaration not allowed
+
+
+// const b; // SYNTAX ERROR
+const b=15;
+// b=10; TYPE ERROR 
+
+// b = 12; // NOT ALLOWED
+console.log(b);
