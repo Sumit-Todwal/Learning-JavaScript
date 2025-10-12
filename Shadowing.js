9db8e2642877e5849ddf1fdf51b205eec1074c66
Sumit Todwal <todwalsumit@gmail.com>
2025-10-12 21:26:06 +0530

    Shadowing in JS

---
+++ Shadowing.js
@@ -0,0 +1,14 @@
+var a = 12;
+let b = 60;
+const c = 80;
+{
+    var a = 10;
+    let b = 90;
+    const c = 72;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+console.log(b);
+console.log(c);
